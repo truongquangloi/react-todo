1594710f435ab375ff5df92b9b5c1d00ccbbb6ab
truongquangloi <loilalung@gmail.com>
2019-01-02 20:28:34 +0700

    #1 init app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//import AppTodo from "./component/app-todo";
+
+var template = <h1>helllo world</h1>;
+ 
+ReactDOM.render(
+  template,
+  document.getElementById('app')
+);
+
+//ReactDOM.render(<Clock />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+    entry: './src/app.js', // File input
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public') // Đuòng dẫn tuyệt đối.
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js/,
+            exclude: /node_modules/,
+        }]
+    }
+}
