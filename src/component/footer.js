68227c174c8d4501cda4ef7456dbab4830af28aa
truongquangloi <loilalung@gmail.com>
2019-01-07 09:53:54 +0700

    #5 refacter header, footer by stateless component

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-class Footer extends React.Component{
-    render() {
-        return(
-            <h3>This is footer</h3>
-        )
-    }
+const Footer = () => {
+    return(
+        <h3>This is footer</h3>
+    )
 }
 
 export default Footer;
--- src/component/header.js
+++ src/component/header.js
@@ -1,11 +1,8 @@
 import React from "react";
 
-class Header extends React.Component{
-    render(){
-        return(        
-            <h3>This is Header</h3>
-        )
-            
-    }
+const Header = () => {
+    return (        
+        <h3>This is Header</h3>
+    )            
 }
 export default Header;
